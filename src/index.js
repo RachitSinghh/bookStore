74b29f43927307549b52d5f49c6d856330a30e05
RachitSinghh <singhrachit@outlook.in>
2025-07-19 23:19:28 +0530

    fix: Correct cron job schedule expression and reorder middleware usage

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ import bookRoutes from "./routes/bookRoutes.js";
 import { connectDB } from "./lib/db.js";
 
 const app = express();
-app.use(cors());
 const PORT = process.env.PORT || 4000;
-// console.log({PORT})
 
 job.start();
-app.use(express.json()); // middleware -> allows you to access the value from the database models [basically parse the json data]
-//routes
+
+app.use(express.json());
+app.use(cors());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 
